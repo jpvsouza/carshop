9e5d7b546802a86b300ddb279de96a6a5f0c3eca
Joao Paulo Veloso <joaopaulox@outlook.com>
2022-10-24 13:59:55 -0300

    feat(Tests): :sparkles: Model test implemented
    functions create, readOne and update implemented

--- src/tests/unit/models/seu-teste-model.test.ts
+++ src/tests/unit/models/seu-teste-model.test.ts
@@ -1,22 +1,56 @@
-// template para criação dos testes de cobertura da camada de model
+import { expect } from 'chai';
+import sinon from 'sinon';
+import CarModel from '../../../models/Car';
+import { Model } from 'mongoose';
+import mongoose from 'mongoose';
+import 'mongoose';
+import { carMock, carMockWithId } from '../mocks/carMock';
 
+describe('Frame Model', () => {
+  const carModel = new CarModel();
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+  before(() => {
+    sinon.stub(Model, 'create').resolves(carMockWithId);
+    sinon.stub(Model, 'findOne').resolves(carMockWithId);
+  });
 
-// describe('Sua descrição', () => {
+  after(() => {
+    sinon.restore();
+  });
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+  describe('creating a car', () => {
+    it('successfully created', async () => {
+      const newCar = await carModel.create(carMock);
+      expect(newCar).to.be.deep.equal(carMockWithId);
+    });
+  });
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+  describe('searching a car', () => {
+    it('successfully found', async () => {
+      const carFound = await carModel.readOne('62cf1fc6498565d94eba52cd');
+      expect(carFound).to.be.deep.equal(carMockWithId);
+    });
 
-//   it('', async () => {});
+    it('car not found', async () => {
+      try {
+        await carModel.readOne('123ERRADO');
+      } catch (error: any) {
+        expect(error.message).to.be.eq('ObjectNotFound');
+      }
+    });
+  });
 
-// });
+  describe('updating a car', () => {
+    it('successfully updated', async () => {
+        sinon.stub(Model, 'findByIdAndUpdate').resolves(carMockWithId);
+        sinon.stub(mongoose, 'isValidObjectId').resolves(true);
+
+        const updatedCar = await carModel.update('idQualquer', carMock);
+
+        expect(updatedCar).to.be.deep.equal(carMockWithId);
+
+        sinon.restore();
+    });
+    it('car not found', () => {});
+  })
+});
